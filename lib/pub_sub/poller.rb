b3f7c293f81df46bdd9a88203c78f9445301a69b
Eugene Mirkin <emirkin@us.westfield.com>
2015-12-17 11:23:55 -0800

    Retry handling a timed-out message

--- lib/pub_sub/poller.rb
+++ lib/pub_sub/poller.rb
@@ -7,7 +7,12 @@ module PubSub
         Breaker.run do
           poller.poll(config) do |message|
             PubSub.logger.debug "PubSub [#{PubSub.config.service_name}] received: #{message.body}"
-            Message.new(message.body).process
+            begin
+              Message.new(message.body).process
+            rescue Faraday::TimeoutError => e
+              Rails.logger.warn e.message
+              throw :skip_delete
+            end
           end
         end
         Breaker.use_next_breaker
